91c55eaa62c898179b7308de819bbd9c8d13f39b
山本卓冬 <numubakusui@gmail.com>
2022-12-14 16:17:17 +0900

    test

---
+++ resources/views/chat/chat.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+  <title>Chat</title>
+</head>
+
+<body>
+  <h1>チャットボット</h1>
+
+  <!--画面-->
+  <div id="chatField">
+    <ul id="chat-ul"></ul>
+  </div>
+
+  <!--入力場所，送信ボタン-->
+  <div id="input-field">
+    <input type="text" id="chat-input">
+    <input type="button" value="Send" id="chat-button" onclick="btnFunc()">
+  </div>
+  <script src="{{ asset('/js/chat.js') }}"></script>
+</body>
+
+</html>
